f2320457a4b3028b87dc7ebebb5c8729a14627f1
vinpinheiro <140824078+vinpinheiro@users.noreply.github.com>
2023-07-28 16:15:31 -0300

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -5,9 +5,8 @@ base = []
 
 a = input("nome: ")
 b = input("ano de nascimento: ")
-c = input("filme favorito: ")
 
 e = 2023 - int(b)
 
-print(f'{a} tem {e} anos e seu filme favorito é {c}')
+print(f'{a} tem {e} anos')
 time.sleep(4)
