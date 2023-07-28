b2d10ced087181dae9e7e22de507df8f577c78ee
Vinicius Pinheiro <viniguep@gmail.com>
2023-07-28 12:00:15 -0300

    nova função do arquivo soma.py e alteração no readme

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
 o programa é responsavel por realizar uma nalise da pessoa de acordo com as inofrmações recebidas
 
-meu primeiro sistema no git alterado e atualizado (!)
+meu primeiro sistema no git alterado e atualizado!
--- soma.py
+++ soma.py
@@ -1,4 +1,10 @@
 a = int(input('digite o valor de a:'))
 b = int(input('digite o valor de b:'))
+cond = input('soma[1] ou subtração[2]? ')
 
-print(f'{a} + {b} = {a+b}')
+if cond == '1':
+    print(f'{a} + {b} = {a+b}')
+elif cond == '2':
+    print(f'{a} - {b} = {a-b}')
+else:
+    print('comando n identificado')
